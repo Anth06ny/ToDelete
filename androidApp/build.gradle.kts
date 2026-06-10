cfc9f6a1ad42801ffb2c403cff32dcb0bfb1ba02
anth0 <anth06ny@gmail.com>
2026-06-10 20:00:15 +0200

    Ajout de la signature

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,9 +1,19 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+
+    id("com.github.gmazzo.buildconfig") version "5.5.1"
+}
+
+// Read API key from local.properties
+val localProperties = Properties() //import java.utils
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
 }
 
 kotlin {
@@ -27,6 +37,17 @@ android {
     namespace = "com.amonteiro.todelete"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
+    //Récupérer les informations du keystore dans local.properties pour signer l'exécutable de sortie
+    //A mettre avant buildTypes
+    signingConfigs {
+        create("release") {
+            storeFile = file(localProperties.getProperty("KEYSTORE_FILE") ?: throw Exception("KEYSTORE_FILE non configuré"))
+            storePassword = localProperties.getProperty("KEYSTORE_PASSWORD") ?: throw Exception("KEYSTORE_PASSWORD non configuré")
+            keyAlias = localProperties.getProperty("KEY_ALIAS") ?: throw Exception("KEY_ALIAS non configuré")
+            keyPassword = localProperties.getProperty("KEY_PASSWORD") ?: throw Exception("KEY_PASSWORD non configuré")
+        }
+    }
+
     defaultConfig {
         applicationId = "com.amonteiro.todelete"
         minSdk = libs.versions.android.minSdk.get().toInt()
@@ -42,6 +63,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
